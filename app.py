96a6318299ff651065e331501cc4459cabc11e0c
Luiz Felippo <coelho.luizfelippo@gmail.com>
2021-04-13 23:02:44 -0300

    Adjusting the layout of the buttons

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ img_list = [ImageTk.PhotoImage(Image.open('testphoto.jpg')), ImageTk.PhotoImage(
             ]
 n = 0
 my_label = Label(image=img_list[n])
-my_label.pack()
+my_label.grid(row=0, column=0, columnspan=3)
 
 
 def forward():
@@ -20,9 +20,9 @@ def forward():
     else:
         n += 1
 
-    my_label.pack_forget()
+    my_label.grid_forget()
     my_label = Label(image=img_list[n])
-    my_label.pack()
+    my_label.grid(row=0, column=0, columnspan=3)
 
 
 def back():
@@ -33,17 +33,17 @@ def back():
     elif n > 0:
         n -= 1
 
-    my_label.pack_forget()
+    my_label.grid_forget()
     my_label = Label(image=img_list[n])
-    my_label.pack()
+    my_label.grid(row=0, column=0, columnspan=3)
 
 
 btn_quit = Button(root, text="Exit Program", command=root.quit)
 btn_forward = Button(root, text=">>", command=forward)
 btn_back = Button(root, text="<<", command=back)
 
-btn_forward.pack()
-btn_back.pack()
-btn_quit.pack()
+btn_forward.grid(row=1, column=2)
+btn_back.grid(row=1, column=0)
+btn_quit.grid(row=1, column=1)
 
 root.mainloop()
